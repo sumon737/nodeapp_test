541ed415c75358f24acb883dd731355a1e075c85
sumon737 <50045219+sumon737@users.noreply.github.com>
2022-09-22 10:56:18 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Sumon-Jenkins-CICD@Alhamdulillah-PodRestartingWithLatestImageTag!!!" }');
+    res.send('{ "response": "Hello From Sumon-Jenkins-CICD@Alhamdulillah-PodRestartingWithLatestImageTag-GoodMorning !!!" }');
 });
 
 app.get('/will', function (req, res) {
