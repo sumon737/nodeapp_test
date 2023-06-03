1090f9e93e082ec2fc1804365f694aa3ca4c8263
sumon737 <50045219+sumon737@users.noreply.github.com>
2023-06-03 23:55:18 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Sumon-Jenkins-CICD@Alhamdulillah-04102022@12.05pm" }');
+    res.send('{ "response": "Hello from Sumon's NodeApp with Jenkins-CICD! Alhamdulillah <3 " }');
 });
 
 app.get('/will', function (req, res) {
